aec34a00d8de1127226c759cfdec91da1850aaa2
bzebosi <zebosibrian@gmail.com>
2026-05-08 09:00:48 -0700

    updates

--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,16 @@ ensure_dir <- function(path) {
   if (!dir.exists(path)) dir.create(path, recursive = TRUE)
   invisible(path)
 }
+
+
+if (!requireNamespace("data.table", quietly = TRUE)) {
+  stop("Package 'data.table' is required.")
+}
+
+naturalsort <- function(x) {
+  if (requireNamespace("stringr", quietly = TRUE)) {
+    stringr::str_sort(as.character(x), numeric = TRUE)
+  } else {
+    sort(as.character(x))
+  }
+}
