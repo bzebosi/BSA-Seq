477f2765f7519d8d1a7059866150b8a0f51cce54
bzebosi <zebosibrian@gmail.com>
2026-09-21 11:39:50 -0700

    Update bash scripts

--- bash/scripts/sbatch_variant.sh
+++ bash/scripts/sbatch_variant.sh
@@ -10,7 +10,7 @@
 
 # Define variables
 env_name="bsa_seq_pkgs"
-script=/nfs5/BPP/Leiboff_Lab/Brian/scripts/BSA-Seq/scripts/run_variant.sh
+script=/nfs5/BPP/Leiboff_Lab/Brian/scripts/BSA-Seq/bash/scripts/run_variant.sh
 
 # Make allocated CPUs available to pipeline
 export threads="${SLURM_CPUS_PER_TASK:-8}"
